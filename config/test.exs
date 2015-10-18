c5b86797abe9a74f523eef30ef379216a85449d0
Gerardo Ortega <geraldavid7@gmail.com>
2015-10-18 16:32:23 -0600

    Initial commit

---
+++ config/test.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :elixir_friends, ElixirFriends.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :elixir_friends, ElixirFriends.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "development_user",
+  password: "12345678",
+  database: "elixir_friends_168_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
